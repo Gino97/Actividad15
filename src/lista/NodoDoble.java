96b3cc5b2451d93e8f10a089c46af60a3771b6a2
francoschroeder <francoschroeder@localhost>
2019-06-13 16:31:01 -0300

    Listillas

---
+++ src/lista/NodoDoble.java
@@ -0,0 +1,65 @@
+package lista;
+
+public class NodoDoble<E> implements Position<E> {
+	private E elem;
+	private NodoDoble<E> next;
+	private NodoDoble<E> prev;
+	
+	/**Crea un NodoDoble con rotulo e y next y prev nulos
+	 * @param e - E
+	 * @author - Liberati Gino y Schroeder Franco
+	 */
+	public NodoDoble(E e) {
+		elem=e;
+		next=null;
+		prev=null;
+	}
+	
+	/**Retorna el rotulo del NodoDoble
+	 * @return elem - E
+	 * @author - Liberati Gino y Schroeder Franco
+	 */
+	public E element() {
+		return elem;
+	}
+	
+	/**Cambia el rotulo del NodoDoble por e
+	 * @param e - E
+	 * @author - Liberati Gino y Schroeder Franco
+	 */
+	public void setElement(E e) {
+		elem=e;
+	}
+	
+	/**Cambia el siguiente del NodoDoble por nod
+	 * @param nod - NodoDoble<E>
+	 * @author - Liberati Gino y Schroeder Franco
+	 */
+	public void setNext(NodoDoble<E> nod) {
+		next=nod;
+	}
+	
+	/**Cambia el anterior del NodoDoble por nod
+	 * @param nod - NodoDoble<E>
+	 * @author - Liberati Gino y Schroeder Franco
+	 */
+	public void setPrev(NodoDoble<E> nod) {
+		prev=nod;
+	}
+	
+	/**Retorna el siguiente del NodoDoble
+	 * @return next - NodoDoble<E>
+	 * @author - Liberati Gino y Schroeder Franco
+	 */
+	public NodoDoble<E> getNext() {
+		return next;
+	}
+	
+	/**Retorna el anterior del NodoDoble
+	 * @return prev - NodoDoble<E>
+	 * @author - Liberati Gino y Schroeder Franco
+	 */
+	public NodoDoble<E> getPrev() {
+		return prev;
+	}
+}
---
+++ src/lista/Position.java
@@ -0,0 +1,9 @@
+package lista;
+
+public interface Position<E> {
+	/**Retorna el rotulo de la position
+	 * @return rotulo - E
+	 */
+	public E element();
+
+}
