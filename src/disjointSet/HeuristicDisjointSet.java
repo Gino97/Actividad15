fa007f09253c11c665a7c3dc0d32033543ac6fb3
AgustinFiore <fiore_agustin@hotmail.com>
2019-06-13 17:32:02 -0300

    disjointset

---
+++ src/disjointSet/HeuristicDisjointSet.java
@@ -0,0 +1,46 @@
+package disjointSet;
+
+public class DisjointSetImp implements DisjointSet{
+
+    private int[] parent;
+    private int[] rank;
+    private int size;
+
+    public DisjointSetImp(int[] nodos){
+        size = 0;
+        parent = new int[nodos.length];
+        for(int i=0; i<nodos.length; i++){
+            makeSet(i);
+            size++;
+        }
+    }
+
+    public int findSet(int nodo){
+        if(nodo!= parent[nodo]){
+            parent[nodo] = findSet(nodo);
+        }
+        return parent[nodo];
+    }
+
+    public void makeSet(int nodo){
+        parent[nodo] = nodo;
+        rank[nodo] = 0;
+    }
+
+    public void union(int x, int y){
+        link(findSet(x), findSet(y));
+        size--;
+    }
+
+    public void link(int x, int y) {
+        if(rank[x]>rank[y]){
+            parent[y] = x;
+        }
+        else{
+            parent[x] = y;
+            if(rank[x]==rank[y]){
+                rank[y] = rank[y] + 1;
+            }
+        }
+    }
+}
