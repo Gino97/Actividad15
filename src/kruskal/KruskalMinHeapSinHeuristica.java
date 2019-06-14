22fddccfb220dba14e6e2c02cfcb43475907b167
francoschroeder <yofrancoschroeder@yahoo.com.ar>
2019-06-13 22:38:55 -0300

    Uno más y no jodemos más

--- src/kruskal/KruskalMinHeapSinHeuristica.java
+++ src/kruskal/KruskalMinHeapSinHeuristica.java
@@ -1,4 +1,34 @@
 package kruskal;
 
+import disjointSet.DisjointSet;
+import disjointSet.HeuristicDisjointSet;
+import grafo.Grafo;
+import grafo.Pesado;
+import lista.ListaDoble;
+import lista.PositionList;
+
+import java.util.List;
+
 public class KruskalMinHeapSinHeuristica {
+    public PositionList<Pesado> kruskal(Grafo grafo) {
+        PositionList<Pesado> listaResultado = new ListaDoble<>();
+        List<Pesado> arcos = grafo.getArcos();
+        Heap heap = new Heap(arcos);
+
+        //Esto debería cambiar para que use conjuntos disjuntos sin heurística
+        DisjointSet conjuntos = new HeuristicDisjointSet(grafo.getNodos());
+
+        while (conjuntos.size()!=1) {
+            Pesado arco = heap.removeMin();
+            int conjNodo1 = conjuntos.findSet(arco.getNodo1());
+            int conjNodo2 = conjuntos.findSet(arco.getNodo2());
+
+            if (conjNodo1 != conjNodo2) {
+                listaResultado.addFirst(arco);
+                conjuntos.union(conjNodo1, conjNodo2);
+            }
+        }
+
+        return listaResultado;
+    }
 }
