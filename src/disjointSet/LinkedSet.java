8db40aa93ced6167203d36c379e37e3f34a89132
AgustinFiore <fiore_agustin@hotmail.com>
2019-06-14 17:31:33 -0300

    ahora si

--- src/disjointSet/LinkedSet.java
+++ src/disjointSet/LinkedSet.java
@@ -1,4 +1,45 @@
 package disjointSet;
 
 public class LinkedSet {
+
+    private Nodo head;
+    private Nodo tail;
+    private int size;
+
+    public LinkedSet(int value){
+        Nodo nodo = new Nodo(value, value, null);
+        head = nodo;
+        tail = nodo;
+        size = 1;
+    }
+
+    public void add(int value){
+        Nodo nodo = new Nodo(value, head.getHead(), null);
+        tail.setNext(nodo);
+        tail = nodo;
+        size++;
+    }
+
+    public int find(int x){
+        Nodo nodo = head;
+        int res = -1;
+        for(int i=0; i<size && res==-1; i++){
+            if(nodo.getValue()==x){
+                res = nodo.getHead();
+            }
+            else{
+                nodo = nodo.getNext();
+            }
+        }
+        return res;
+    }
+
+    public Nodo getHead(){
+        return head;
+    }
+
+    public int size(){
+        return size;
+    }
+
 }
--- src/disjointSet/Nodo.java
+++ src/disjointSet/Nodo.java
@@ -1,4 +1,31 @@
 package disjointSet;
 
 public class Nodo {
+
+    private int value;
+    private int head;
+    private Nodo next;
+
+    public Nodo(int value, int head, Nodo next){
+        this.value = value;
+        this.head = head;
+        this.next = next;
+    }
+
+    public int getValue(){
+        return value;
+    }
+
+    public Nodo getNext(){
+        return next;
+    }
+
+    public int getHead(){
+        return head;
+    }
+
+    public void setNext(Nodo nodo){
+        next = nodo;
+    }
+
 }
